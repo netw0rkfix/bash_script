5366f1a9ff75df2c6ab78d822ac1c30218b0a4ec
netw0rkfix <110713013+netw0rkfix@users.noreply.github.com>
2022-08-06 02:43:54 -0400

    Delete add_zsh_aliases.sh

--- add_zsh_aliases.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#alias reload='source ~/.zshrc'
-#alias sshcon='ssh -C -p 6697 -l maxime -L 5904:127.0.0.1:5904 con.oc9.com'
-#alias cha2='autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -p 6697 -L:8006:127.0.0.1:8006 root@charlotte2.oc9.com'
