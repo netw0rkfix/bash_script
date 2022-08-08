312956389525c673ba1c88bbf86d8881ee58c212
Maxime <maxime@networkfix.org>
2022-08-07 21:04:47 -0400

    update spaceship

--- spaceship_prompt.sh
+++ spaceship_prompt.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
-### This script need to be run as root and in the root folder
-### Install ZSH && oh-my-zsh
-apt-get install fonts-powerline -y
-apt install zsh -y
+
+### Install ZSH && oh-my-zsh + spaceship theme
+apt-get install fonts-powerline git rsync zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-### Set theme in .zshrc
 sed -i 's/robbyrussell/spaceship/g' ~/.zshrc
-#rsync -r -e "ssh" --delete -av root@dc01:~/.oh-my-zsh/custom/plugins/ ~/.oh-my-zsh/custom/plugins/
-#sed -i 's/git/git k zsh-autosuggestions zsh-completions zsh-syntax-highlighting nmap ctf pip systemd sudo/g' ~/.zshrc
-### Install p10K && spaceship themes
+rsync -r -e "ssh" --delete -av (username)@(host):~/.oh-my-zsh/custom/plugins/ ~/.oh-my-zsh/custom/plugins/
+sed -i 's/git/git k zsh-autosuggestions zsh-completions zsh-syntax-highlighting/g' ~/.zshrc
 cd ~/root/.oh-my-zsh/custom/themes
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship-prompt --depth=1
 ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
 
-
-
-
-
-****
-### install zsh + spaceship
-#apt-get install zsh fonts-powerline -y
-#chsh -s $(which zsh)
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#rsync -r -e "ssh -p 65531" --delete -av root@homelab.technoptik.com:~/.oh-my-zsh/custom/plugins/ ~/.oh-my-zsh/custom/plugins/
-#sed -i 's/robbyrussell/spaceship/g' ~/.zshrc
-#sed -i 's/git/git k zsh-autosuggestions zsh-completions zsh-syntax-highlighting nmap ctf pip systemd sudo/g' ~/.zshrc
-#git clone https://github.com/spaceship-prompt/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship-prompt --depth=1
-#ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
